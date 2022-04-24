34cf5e566808d8cef5b636ac56e4b44e5c8261d1
nataliazo <natalya.zosimova@gmail.com>
2022-04-24 00:26:10 -0400

    add exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,23 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     #объявляем конструктор __init__ В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    # реализуем метод is_element_present, в котором будем перехватывать исключение. В него будем передавать два аргумента:
+    # как искать (css, id, xpath и тд) и собственно что искать (строку-селектор);
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
     #добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get().
     def open (self):
