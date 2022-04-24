f33debda19e66b46003fa07f72d194f7fe81d0ad
nataliazo <natalya.zosimova@gmail.com>
2022-04-23 22:46:06 -0400

    full merge

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: ru,en,...(ect)")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    options = Options()
+    if browser_name == "chrome":
+
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(executable_path= "C:\chromedriver\chromedriver.exe", options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(executable_path="C:\geckodriver\gekodriver.exe", firefox_profile=fp)
+    else:
+        print("Browser <browser_name> still is not implemented")
+
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
